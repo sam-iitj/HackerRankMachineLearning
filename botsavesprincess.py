e50e330b7168ea51bf38d2b3b6bded34278bdbbb
sam_iitj <pg201272009@iitj.ac.in>
2016-10-01 17:43:20 +0530

    Correctness

--- botsavesprincess.py
+++ botsavesprincess.py
@@ -12,16 +12,16 @@ def displayPathtoPrincess(n,grid):
     while start_pos != target_pos:
         if target_pos[1] - start_pos[1] > 0:
             print "RIGHT"
-            start_pos[1] += start_pos[1]
+            start_pos[1] = start_pos[1] + 1
         elif target_pos[1] - start_pos[1] < 0:
             print "LEFT"
-            start_pos[1] -= start_pos[1]
+            start_pos[1] = start_pos[1] - 1
         elif target_pos[0] - start_pos[0] < 0:
             print "UP"
-            start_pos[0] -= start_pos[0]
+            start_pos[0] = start_pos[0] - 1
         elif target_pos[0] - start_pos[0] > 0:
             print "DOWN"
-            start_pos[0] += start_pos[0]
+            start_pos[0] = start_pos[0] + 1
 
 #print all the moves here
 m = input()
